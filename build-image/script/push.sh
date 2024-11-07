8770287c1869a1d6f0f47b01eee05b65ecaf078a
hiro345g <hiro345g@gmail.com>
2024-11-07 14:50:05 +0900

    dvc-novnc-mise 追加

--- build-image/script/push.sh
+++ build-image/script/push.sh
@@ -3,6 +3,7 @@ VERSION=202411
 USER_NAME=hiro345g
 docker push ${USER_NAME}/dvc:base-${VERSION}
 docker push ${USER_NAME}/dvc:novnc-${VERSION}
+docker push ${USER_NAME}/dvc:novnc-mise-${VERSION}
 docker push ${USER_NAME}/dvc:novnc-go-${VERSION}
 docker push ${USER_NAME}/dvc:novnc-jdk-${VERSION}
 docker push ${USER_NAME}/dvc:novnc-php-${VERSION}
